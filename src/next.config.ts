3439e467e234d5dd7104d75d62e339738bed84f3
andres felipe contreras rubiano <workeraplicationservices@gmail.com>
2025-06-14 02:12:21 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,38 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1748657581481.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev',
+    ],
+  },
+  webpack: (config, { isServer, webpack }) => {
+    // Alias Handlebars to its precompiled version to avoid 'require.extensions' error with Webpack
+    // This is a common workaround for projects using Handlebars with Webpack.
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'handlebars': 'handlebars/dist/handlebars.js',
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
